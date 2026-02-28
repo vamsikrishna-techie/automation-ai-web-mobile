df859a857a8931faf60c73e845ff243ef964708b
Vamsi Krishna <vamsi.vk.1608@gmail.com>
2026-02-28 10:47:47 -0800

    Fix CI: run Playwright headless in Git actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     testIdAttribute: 'data-test',
-    headless: false,
+    headless: process.env.CI ? true : false,
     launchOptions: {
       slowMo: 800,
     },
